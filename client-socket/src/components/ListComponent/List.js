5acffab3dfb9542792b17ae322fdc7e6f8050639
sitobusnan <sito@citytonight.es>
2019-12-06 16:06:29 +0100

    fix fix fix

--- client-socket/src/components/ListComponent/List.js
+++ client-socket/src/components/ListComponent/List.js
@@ -11,7 +11,10 @@ export default class List extends Component {
       movies: []
     };
 
+    // Recibimos el socket por props, se creó en ChatComponent.js
     this.socket = this.props.socket;
+
+    // Creamos un ".on" que escuchará las listas actualizadas
     this.socket.on("moviesList", moviesList => {
       this.setState({ ...this.state, movies: moviesList });
     });
@@ -22,20 +25,29 @@ export default class List extends Component {
     this.setState({ [name]: value });
   };
 
+  // A partir de la información del formulario, genera una nueva "pelicula"
   handlerSubmit = e => {
     e.preventDefault();
     let movie = {
       title: this.state.title,
       image: this.state.image
     };
+
+    // Este ".emit" le envia al server las "peliculas" creadas en el formulario
+    // El server se encargará de propagarlas
     this.socket.emit("newMovie", movie);
     this.setState({ ...this.state, title: "", image: "" });
   };
 
+
+  // Este componente renderiza el formulario de nueva "pelicula"
+  // y renderiza y actualiza la lista
   render() {
     return (
       <div>
         <h1>MOVIES LIST</h1>
+
+        {/* Formulario para nueva pelicula */}
         <Form id="taskForm">
           <Form.Group controlId="formBasicText">
             <Form.Label>Title</Form.Label>
@@ -49,7 +61,6 @@ export default class List extends Component {
               value={this.state.title}
             />
           </Form.Group>
-
           <Form.Group controlId="formBasicText">
             <Form.Label>Image Link</Form.Label>
             <Form.Control
@@ -72,10 +83,12 @@ export default class List extends Component {
             Submit
           </Button>
         </Form>
+
+        {/* Lista actualizable de peliculas */}
         <div id="listGroup">
           {this.state.movies.map((elem, idx) => {
             return (
-              <Row id="line">
+              <Row id="line" key={idx}>
                 <Col xs={6} md={4}>
                   <Image id="img-row" src={elem.image} rounded />
                 </Col>
